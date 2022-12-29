d6f2ceae89d24c4e107ba42434af3f4b92255153
Mouad Bajjouk <mouad.bajjouk@terabyte-software.com>
2022-12-30 00:04:39 +0100

    added angular material

--- BookStoreApp/src/app/app.component.ts
+++ BookStoreApp/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  email = 'a@a.com'
   title = 'BookStoreApp';
 }
--- BookStoreApp/src/app/app.module.ts
+++ BookStoreApp/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { HowItWorksComponent } from './how-it-works/how-it-works.component';
+// import { AuthModule } from './auth/auth.module';
+// import { UserModule } from './user/user.module';
+// import { PublicModule } from './public/public.module';
+import { SharedModule } from './shared/shared.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, AboutUsComponent, HowItWorksComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    // AuthModule,
+    // UserModule,
+    // PublicModule,
+    SharedModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
