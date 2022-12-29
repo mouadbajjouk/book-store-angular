d1eef1f727785994a9b433cb0850005f5aa4b8d6
Mouad Bajjouk <mouad.bajjouk@terabyte-software.com>
2022-12-30 00:05:25 +0100

    just angular learning

---
+++ BookStoreApp/src/app/auth/components/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  public name: string = '';
+  constructor(private route: ActivatedRoute, private router: Router) {
+
+  }
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(queryParam => {
+      console.log(queryParam);
+      this.name = queryParam['name'];
+    })
+  }
+
+  goToSignup(): void {
+    this.router.navigate(['auth/signup']);
+  }
+
+  goToBookDetails(id: number, authorId: number): void {
+    this.router.navigate(['public/book-details', id, 'author', authorId],
+      {
+        queryParams: { name: 'mouad', email: 'a@a.com' }
+      });
+  }
+}
---
+++ BookStoreApp/src/app/user/user.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit{
+  ngOnInit(): void {
+  }
+  
+}
